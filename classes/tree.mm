1fde4c3eb31c88bd44e66879dfc26696c62a61e0
CancerHermit <cancerhermit@gmail.com>
2012-05-17 20:18:48 +0400

    add new

---
+++ classes/tree.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1334848594750" ID="ID_1399387238" MODIFIED="1334848594750" TEXT="New Mindmap"/>
+</map>
